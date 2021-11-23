8a13e87378f4fe14986180c092d5614c63d0a232
arimohy <arimohy@gmail.com>
2021-11-23 12:24:30 -0500

    creacion de reducer de trainingActivos

---
+++ src/state/ActiveTraining/activeTrainingReducer.js
@@ -0,0 +1,21 @@
+import * as actions from "./programAction";
+
+export const initialState = {
+    trainingActivos: [],
+    idtrainingseleted: null,
+    hasErrors: false,
+    loading: false,
+};
+
+export default function programReducer(state = initialState, action) {
+    switch (action.type) {
+        case actions.LOADING:
+        return { ...state, loading: true };
+        case actions.LOADED_SUCCESS:
+        return { ...state, ...action.payload, loading: false };
+        case actions.LOADED_FAILURE:
+        return { ...state, loading: false, hasErrors: true };
+        default:
+        return state;
+    }
+}
