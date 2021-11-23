7c4cb9cad70d05f4d389ded8615f198207bc98c9
sirlocust <jua56yu@hotmail.com>
2021-11-23 10:24:12 -0500

    se agrega el reducer y thunkactios al state para course #16

---
+++ src/thunkAction/coursesThunk.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+export const setAllCourses = createAsyncThunk(
+  'courses/getAllCourses',
+  async () => {
+    const response = await 'function api';
+    return response;
+  }
+);
+
+export const deleteCourse = createAsyncThunk(
+  'courses/deleteCourse',
+  async () => {
+    const response = await 'function api';
+    return response;
+  }
+);
+
+export const putCourse = createAsyncThunk('courses/putCourse', async () => {
+  const response = await 'function api';
+  return response;
+});
+
+export const addCourse = createAsyncThunk('courses/addCourse', async () => {
+  const response = await 'function api';
+  return response;
+});
