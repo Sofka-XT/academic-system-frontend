22c2a28d2bffae5ba12a77cc91efc4a8ec7954c9
Santiago Franco <ktns0930@gmail.com>
2021-11-23 18:53:45 -0500

    Little irrelevant changes. #30

--- src/pages/dashboard/components/Navbar.jsx
+++ src/pages/dashboard/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar( { user } ) {
     console.log(user);
     return (
         <div className = "Navbar">
-            <img src = "https://i.imgur.com/kmc2s6p.png"></img>
+            <img src = "https://i.imgur.com/kmc2s6p.png" className = "sofka-logo"></img>
             <div className = "right-side">
                 <h6>{user.tipo}</h6>
                 <h5>{user.name}</h5>
---
+++ src/pages/dashboard/components/welcome/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Welcome() {
+    return (
+        <div>
+            <h1>Hola</h1>
+            <h2>Mundo</h2>
+            <h3>Soy Santi</h3>
+        </div>
+    )
+}
