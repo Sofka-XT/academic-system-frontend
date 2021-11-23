508a56366ed694f3b2b8a7ea8d4b40150933b1b5
jeantr35 <jean.trujillo.35@gmail.com>
2021-11-23 15:40:52 -0500

    Added the type to the user #34

--- src/thunkAction/authThunk.js
+++ src/thunkAction/authThunk.js
@@ -1,13 +1,34 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { loginGoogle, signOutFirebase } from '../api/auth/authRequest';
+import { doc, getDoc } from "firebase/firestore";
+import { db } from '../config/firebase/firebase.config';
+
 export const loginWhitGoogle = createAsyncThunk(
   'auth/loginWhitGoogleStatus',
   async () => {
     const response = await loginGoogle();
-    return response.user.uid;
+
+  const docRef = doc(db, "coach", response.user.uid);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    const data = {
+      id : response.user.uid,
+      tipo: docSnap.data().tipo
+    }
+    return data;
   }
+  return {
+    id : response.user.uid,
+    tipo: "ESTUDIANTE"
+  }
+}
 );
 
 export const signOut = createAsyncThunk('auth/signOutStatus', async () => {
   return await signOutFirebase();
 });
+
+
+const getTipoDeUsuario = (uid) => {
+  
+}
