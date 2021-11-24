74ab64358397554d0d6349a432ad5584313873a8
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-24 15:16:23 -0500

    HU#7/Created form for input a new training

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,26 @@
-import { HomePageComponent } from '../pages/home/HomePageComponent';
-import { DashBoardPageComponent } from './../pages/dashboard/DashBoardPageComponent';
-
+import { HomePageComponent } from "../pages/home/HomePageComponent";
+import { DashBoardPageComponent } from "./../pages/dashboard/DashBoardPageComponent";
+import FormInputTrainingComponent from "./../pages/crudTraning/components/FormInputTrainingComponent";
 export const routesApp = [
   {
-    path: '/home',
-    name: 'Home',
+    path: "/home",
+    name: "Home",
     component: <HomePageComponent />,
   },
   {
-    path: '/dashboard',
-    name: 'DashBoard',
+    path: "/dashboard",
+    name: "DashBoard",
     component: <DashBoardPageComponent />,
     child: [
       {
-        path: 'casa',
-        name: 'casa',
+        path: "casa",
+        name: "casa",
         component: <HomePageComponent />,
+      },
+      {
+        path: "createTraining",
+        name: "formInputTraining",
+        component: <FormInputTrainingComponent />,
       },
     ],
   },
