63da94239f8f2d108cb632a52fcd75e1966599eb
Raul A. Alzate <alzategomez.raul@gmail.com>
2021-11-24 08:44:06 -0500

    ADD env

--- src/environments/enviroment.js
+++ src/environments/enviroment.js
@@ -1,3 +1,11 @@
+export const FB_API_KEY = process.env.REACT_APP_API_FB_KEY;
+export const FB_AUTH_DOMAIN = process.env.REACT_APP_FB_AUTH_DOMAIN;
+export const FB_DATABASE_URL = process.env.REACT_APP_FB_DATABASE_URL;
+export const FB_PROJECT_ID = process.env.REACT_APP_FB_PROJECT_ID;
+export const FB_STORAGE_BUCKET = process.env.REACT_APP_STORAGE_BUCKET;
+export const FB_MESSAGING_SENDER_ID = process.env.REACT_APP_FB_MESSAGING_SENDER_ID;
+export const HOST_API = process.env.REACT_APP_HOST_API;
+
 export const enviroment = {
   firabase: {
     apiKey: 'AIzaSyDAGnCkXYQf_toW_TqV4n6GDBUiYOzMsMw',
@@ -11,3 +19,5 @@ export const enviroment = {
     messagingSenderId: '27962594566',
   },
 };
+
+console.log(process.env);
