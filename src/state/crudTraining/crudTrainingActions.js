867430d12ef2de64c250cb82ccac12328f791a12
Santiago Posada <santiago90dell@gmail.com>
2021-11-25 09:25:34 -0500

    Fixed som issuesin the raining actions

--- src/state/crudTraining/crudTrainingActions.js
+++ src/state/crudTraining/crudTrainingActions.js
@@ -4,13 +4,12 @@ export const LOADING = "LOADING";
 export const LOADED_SUCCESS = "LOADED_SUCCESS";
 export const LOADED_FAILURE = "LOADED_FAILURE";
 export const POST_PROGRAM_SUCCESS = "POST_PORGRAM_SUCCES";
-export const 
 
 export const loading = () => ({ type: LOADING });
 
 export const postTrainingSuccess = (payload) => ({
-  type: POST_PROGRAM_SUCCES,
-  payload,
+  type: POST_PROGRAM_SUCCESS,
+  payload
 });
 
 export const failure = () => ({ type: LOADED_FAILURE });
