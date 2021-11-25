d68690c964b4b73c368aaa83472d2ff43e8a2cb9
sirlocust <jua56yu@hotmail.com>
2021-11-25 09:55:21 -0500

    fix merge 2

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,11 @@
 import { HomePageComponent } from '../pages/home/HomePageComponent';
-<<<<<<< HEAD
 import Welcome from '../pages/dashboard/components/welcome/Welcome.jsx';
+
 import {
   DashBoardPageComponent,
   DashBoardApprenticeComponent,
 } from './../pages/dashboard/DashBoardPageComponent';
-=======
-import Welcome from '../pages/dashboard/components/welcome/Welcome.jsx'
-import { DashBoardPageComponent, DashBoardApprenticeComponent } from './../pages/dashboard/DashBoardPageComponent';
 import TrainingListPageComponent from '../pages/trainingListPage/TrainingListPageComponent.jsx';
->>>>>>> 956e62f406fe53742a6a737d5f929233ddf50992
 
 import { CreateCoursePageComponent } from './../pages/createCourse/CreateCoursePageComponent';
 import EditCoursePageComponent from './../pages/createCourse/Components/EditCoursePageComponent';
@@ -84,53 +80,53 @@ export const routesApp = [
     ],
   },
 
-	{
-		path: '/dashboard',
-		name: 'DashBoard',
-		component: <DashBoardPageComponent />,
-		child: [
-			{
-				path: 'home',
-				name: 'Home',
-				component: <Welcome />,
-			},
-			{
-				path: 'logout',
-				name: 'Log Out',
-				component: <HomePageComponent />,
-			},
-			{
-				path: 'trainingstory',
-				name: 'Training Story',
-				component: <h1>TRAINING STORY</h1>,
-			},
-			{
-				path: 'activeprogram',
-				name: 'Active Program',
-				component: <h1>Active Program</h1>,
-			},
-			{
-				path: 'program',
-				name: 'Create Program',
-				component: <h1>Create Program</h1>,
-			},
-			{
-				path: 'programstory',
-				name: 'Program Story',
-				component: <h1>Program Story</h1>,
-			},
-			{
-				path: 'activetrainingcoach', 
-				name: 'Active Training',
-				component: <TrainingListPageComponent/>,
-			},
-			{
-				path: 'training',
-				name: 'Create Training',
-				component: <h1>Create Training</h1>,
-			},
-		],
-	}
+  {
+    path: '/dashboard',
+    name: 'DashBoard',
+    component: <DashBoardPageComponent />,
+    child: [
+      {
+        path: 'home',
+        name: 'Home',
+        component: <Welcome />,
+      },
+      {
+        path: 'logout',
+        name: 'Log Out',
+        component: <HomePageComponent />,
+      },
+      {
+        path: 'trainingstory',
+        name: 'Training Story',
+        component: <h1>TRAINING STORY</h1>,
+      },
+      {
+        path: 'activeprogram',
+        name: 'Active Program',
+        component: <h1>Active Program</h1>,
+      },
+      {
+        path: 'program',
+        name: 'Create Program',
+        component: <h1>Create Program</h1>,
+      },
+      {
+        path: 'programstory',
+        name: 'Program Story',
+        component: <h1>Program Story</h1>,
+      },
+      {
+        path: 'activetrainingcoach',
+        name: 'Active Training',
+        component: <TrainingListPageComponent />,
+      },
+      {
+        path: 'training',
+        name: 'Create Training',
+        component: <h1>Create Training</h1>,
+      },
+    ],
+  },
 ];
 
 export const apprenticeRoutesApp = [
