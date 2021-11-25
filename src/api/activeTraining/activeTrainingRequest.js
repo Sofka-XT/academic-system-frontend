8aada45d532946461f1c32a58e7f6654a6b3be94
Andres0x90 <andrescaza246@hotmail.com>
2021-11-25 14:47:15 -0500

    Local api link variable changed to global enviroment refereced to #35

--- src/api/activeTraining/activeTrainingRequest.js
+++ src/api/activeTraining/activeTrainingRequest.js
@@ -1,7 +1,8 @@
+import { enviroment } from "../../environments/enviroment"
 //url para prueba cambiar al tener la url real
 //const URL_BASE = 'https://jsonplaceholder.typicode.com/users';
 // const URL_BASE='https://vast-everglades-55825.herokuapp.com/training/list-actives'
-const URL_BASE='https://vast-everglades-55825.herokuapp.com';
+const URL_BASE=enviroment.host;
 //const URL_BASE='http://localhost:8080/training/list-actives'
 export const getActiveTrainingApi = () => {
     return fetch(`${URL_BASE}/training/list-actives`)
