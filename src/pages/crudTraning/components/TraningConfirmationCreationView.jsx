76df52d7658ef577472f9bc693307a9713f5e6b6
Juan Camilo Cardona Calderon <juan.cardona69@udea.edu.co>
2021-11-26 02:59:00 -0500

    Implemented styles for TraningConfirmationCreationView

---
+++ src/pages/crudTraning/components/TraningConfirmationCreationView.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import CSVTableComponent from "./CSVTableComponent";
+
+const TraningConfirmationCreationView = ({ setFormSent }) => {
+  const { traningSent } = useSelector((state) => state.crudTrainingReducer);
+  const handleNewTraning = (e) => {
+    setFormSent(false);
+  };
+
+  return (
+    <div className="screen-container text-center">
+      <div className="">
+        <div className="title-container lecontainer">
+          <h2 className="my-3">Traning Creado Exitosamente</h2>
+          <hr />
+        </div>
+        <h5 className="text-center mt-3">nombre: {traningSent.name}</h5>
+        <h5 className="text-center my-3">
+          Empieza en la fecha: {traningSent.startingDate}
+        </h5>
+
+        <h4 className="text-center mt-3">
+          Lista de coaches asignados para el training
+        </h4>
+        <CSVTableComponent data={traningSent.coaches} />
+
+        <h4 className="text-center mt-5">
+          Lista de aprendices asignados para el training
+        </h4>
+        <CSVTableComponent data={traningSent.apprentices} />
+        {/* <h3 className="text-center">Program: PENDIENTE</h3> */}
+      </div>
+      <button
+        id="traning__new-traning-btn"
+        className="btn btn-primary btn-new-traning"
+        onClick={handleNewTraning}
+      >
+        Crear un nuevo training
+      </button>
+    </div>
+  );
+};
+
+export default TraningConfirmationCreationView;
