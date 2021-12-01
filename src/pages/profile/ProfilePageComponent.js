d2a3051cbbe58d5c2f9bf8396e96f4f513bb6369
mvm11 <mvalenciaminota@gmail.com>
2021-12-01 07:50:53 -0500

    Some styles added

--- src/pages/profile/ProfilePageComponent.js
+++ src/pages/profile/ProfilePageComponent.js
@@ -2,21 +2,29 @@ import { useEffect } from "react";
 import { connect } from "react-redux";
 import { useAppDispatch } from "../../state/store.hook";
 import { getApprenticeInfo } from "../../thunkAction/profileThunk";
+import ApprenticeProfile from "./components/ApprenticeProfile";
 
-const  ProfilePageComponent = ({apprentice}) =>
-{
-    const dispatch = useAppDispatch();
+const ProfilePageComponent = ({ apprentice }) => {
+  const dispatch = useAppDispatch();
 
-    useEffect(()=>
-    {
-        dispatch(getApprenticeInfo());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(getApprenticeInfo());
+  }, [dispatch]);
 
-    console.log(apprentice);
-    return <div></div>;
-}
+  console.log(apprentice);
+  return (
+    <>
+      {apprentice && (
+        <div>
+          <h1>Profiles</h1>
+          <ApprenticeProfile profile={apprentice}></ApprenticeProfile>
+        </div>
+      )}
+      ;
+    </>
+  );
+};
 
-const mapState = (state) => 
-({apprentice: state.profileReducer.apprentice})
+const mapState = (state) => ({ apprentice: state.profileReducer.apprentice });
 
 export default connect(mapState)(ProfilePageComponent);
---
+++ src/pages/profile/components/ApprenticeProfile.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./ApprenticeProfile.css";
+
+const ApprenticeProfile = ({ profile }) => {
+  return (
+    <>
+      {profile && (
+        <div class="ApprenticeProfileContainer">
+          <div class="profile">
+            <div class="profile-left">
+              <img
+                src="http://assets.stickpng.com/images/585e4beacb11b227491c3399.png"
+                alt="profile-pic"
+              />
+            </div>
+
+
+
+          <div class="profile-datos">
+
+
+            <div>
+              <h1>{profile.apprenticeName}</h1>
+            </div>
+
+            <div class="profile-data">
+              <ul>
+                <li>
+                  <span class="profile-stat-count">Celular: </span>{" "}
+                  {profile.phoneNumber}
+                </li>
+                <li>
+                  <span class="profile-stat-count">Correo: </span>
+                  {profile.email}
+                </li>
+              </ul>
+            </div>
+
+
+            </div>
+
+
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default ApprenticeProfile;
