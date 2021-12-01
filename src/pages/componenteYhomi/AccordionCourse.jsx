eb37c982021f89ebbc3ec5f1a27dc6c6e21e4cb9
arimohy <arimohy@gmail.com>
2021-12-01 06:55:08 -0500

    Modificacion de estilos accordion #4

--- src/pages/componenteYhomi/AccordionCourse.jsx
+++ src/pages/componenteYhomi/AccordionCourse.jsx
@@ -18,8 +18,8 @@ const AccordionCourse = ({courses}) => {
             {courses.map((item,i)=>(
                 <div className="item">
                     <div className ='title' on onClick={()=>toggle(i)}>
-                        <h2>{item.name}</h2>
-                        <span>{selected===i?'-':'+'}</span>
+                        <h2 className='titleAccordionName'>{item.name}</h2>
+                        <i className={selected===i?"fas fa-minus-circle":"fas fa-plus-circle"}></i>
                     </div>
                     <div className= {selected===i?'content show':'content'}>
                     {/*item.categories[0].name*/}
